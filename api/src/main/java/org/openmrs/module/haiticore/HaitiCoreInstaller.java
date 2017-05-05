3459359977a8584f7d35e0d11ec0038a1769edbf
Craig Appl <cappl@uw.edu>
2017-05-05 16:03:53 -0700

    Adding Biometric Patient Identifier so we can begin to use it for the M2Sys fingerprint scanner development

--- api/src/main/java/org/openmrs/module/haiticore/HaitiCoreInstaller.java
+++ api/src/main/java/org/openmrs/module/haiticore/HaitiCoreInstaller.java
@@ -4,6 +4,7 @@ import org.openmrs.api.context.Context;
 import org.openmrs.module.haiticore.metadata.HaitiAddressBundle;
 import org.openmrs.module.haiticore.metadata.HaitiEncounterTypeBundle;
 import org.openmrs.module.haiticore.metadata.HaitiPersonAttributeTypeBundle;
+import org.openmrs.module.haiticore.metadata.HaitiPatientIdentifierTypeBundle;
 import org.openmrs.module.metadatadeploy.api.MetadataDeployService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -18,6 +19,7 @@ public class HaitiCoreInstaller {
         metadataDeployService.installBundle(Context.getRegisteredComponents(HaitiPersonAttributeTypeBundle.class).get(0));
         metadataDeployService.installBundle(Context.getRegisteredComponents(HaitiEncounterTypeBundle.class).get(0));
         metadataDeployService.installBundle(Context.getRegisteredComponents(HaitiAddressBundle.class).get(0));
+        metadataDeployService.installBundle(Context.getRegisteredComponents(HaitiPatientIdentifierTypeBundle.class).get(0));
     }
 
 }
---
+++ api/src/main/java/org/openmrs/module/haiticore/metadata/HaitiPatientIdentifierTypes.java
@@ -0,0 +1,30 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
+package org.openmrs.module.haiticore.metadata;
+
+import org.openmrs.module.metadatadeploy.descriptor.PatientIdentifierTypeDescriptor;
+
+/**
+ * Constants for all defined patient identifier types
+ */
+public class HaitiPatientIdentifierTypes {
+
+	public static PatientIdentifierTypeDescriptor BIOMETRIC_REF_NUMBER = new PatientIdentifierTypeDescriptor() {
+		public String uuid() { return "e26ca279-8f57-44a5-9ed8-8cc16e90e559"; }
+		public String name() { return "Biometrics Reference Code"; }
+		public String description() { return "Code referencing a patient's record in an external biometrics system"; }
+	};
+
+}
